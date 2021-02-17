6444d4a52b0b6d5b66d6f027438397220d409e7f
Travis Lowe <travis.lowe@redcanary.com>
2021-02-17 11:37:05 -0600

    setup

---
+++ Linux-Series/File-Access-Control/assets/.setup.sh
@@ -0,0 +1,6 @@
+!#/bin/bash
+echo "Getting everything setup, 1 moment"
+
+chmod 755 file1.sh
+chmod 600 file2.sh
+chown root file3.sh
--- Linux-Series/File-Access-Control/setup.sh
+++
@@ -1,1 +0,0 @@
-sleep 3; echo "Hello"; touch file2.sh; chmod 755 file2.sh
